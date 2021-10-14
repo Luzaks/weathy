77acd3a2f77d310df4cb55e69520ec6226a43c7c
Luzaks <kanemekanik@gmail.com>
2021-10-14 18:16:37 -0500

    feat(development): add needed error page

--- src/components/pages/ErrorPage/index.tsx
+++ src/components/pages/ErrorPage/index.tsx
@@ -1,15 +1,57 @@
-import React from 'react';
-import {Link,RouteComponentProps} from 'react-router-dom';
+import React from "react";
+import { Link, RouteComponentProps } from "react-router-dom";
+import { Grid } from "@mui/material";
 
-interface ErrorPageProps extends RouteComponentProps{
+import { PrimaryText, PrimaryBox } from "../../atoms";
+
+interface ErrorPageProps extends RouteComponentProps {
   id: string;
 }
 
 export const ErrorPage: React.FC<ErrorPageProps> = () => {
-  return(
-    <>
-      <Link to={'/'}><p>Login</p></Link>
-      <h1>Error Page</h1>
-    </>
+  return (
+    <Grid
+      container
+      width={"100vw"}
+      height={"100vh"}
+      direction={"column"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      alignSelf={"center"}
+    >
+      <PrimaryBox
+        sx={{
+          width: "70%",
+          height: "80%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+          borderRadius: 4,
+        }}
+      >
+        <PrimaryText
+          content={"Ups..."}
+          variant={"h3"}
+          fontSize={32}
+          color={"#fff"}
+        />
+        <PrimaryText
+          content={"Page not found"}
+          variant={"h1"}
+          fontSize={64}
+          color={"#fff"}
+        />
+        <Link to={"/"}>
+          <PrimaryText
+            content={"go to login"}
+            variant={"span"}
+            fontSize={14}
+            color={"#fff"}
+          />
+        </Link>
+      </PrimaryBox>
+    </Grid>
   );
 };
