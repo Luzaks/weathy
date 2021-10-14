e4908163461d58779afb856c199a913e022eb531
Luzaks <kanemekanik@gmail.com>
2021-10-14 17:24:29 -0500

    feat(development): add Cards template

---
+++ src/components/templates/CardsTemplate/index.tsx
@@ -0,0 +1,32 @@
+import React, { FC } from "react";
+import { Box } from "@mui/material";
+
+import { PrimaryLoader } from "../../atoms";
+import { WeatherCard } from "../../molecules";
+
+interface CardsTemplateProps {
+  data: any[];
+  isLoading: boolean;
+}
+
+export const CardsTemplate: FC<CardsTemplateProps> = ({ data, isLoading }) => {
+  return (
+    <Box
+      sx={{
+        width: "100vw",
+        display: "flex",
+        flexDirection: "row",
+        flexWrap: "wrap",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 5,
+      }}
+    >
+      {isLoading && <PrimaryLoader />}
+      {!isLoading &&
+        data.map((item, index) => {
+          return <WeatherCard key={index} cityInfo={item} />;
+        })}
+    </Box>
+  );
+};
--- src/components/templates/index.tsx
+++ src/components/templates/index.tsx
@@ -1,1 +1,1 @@
-export {};
+export * from "./CardsTemplate";
