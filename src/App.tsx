00799481d68811b96287c58e230d9a88ea427bac
Luzaks <kanemekanik@gmail.com>
2021-10-14 17:48:36 -0500

    feat(development): integrate router

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,15 @@
 import React from "react";
+import { ThemeProvider } from "@mui/styles";
+
+import { RouterRoutes } from "./integrations";
+import { principalTheme, GlobalStyles } from "./models";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={principalTheme}>
+      <RouterRoutes />
+      <GlobalStyles />
+    </ThemeProvider>
   );
 }
 
--- src/integrations/index.ts
+++ src/integrations/index.ts
@@ -1,2 +1,3 @@
 export * from "./auth";
 export * from "./weather-api";
+export * from "./router";
